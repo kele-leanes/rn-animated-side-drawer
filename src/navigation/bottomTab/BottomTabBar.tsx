a7531013066f7924f449f846ba40192b21382220
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2024-01-26 17:16:25 -0300

    feat: add DrawerScreenWrapper and DrawerContent to BottomTabBar and SideDrawer

--- src/navigation/bottomTab/BottomTabBar.tsx
+++ src/navigation/bottomTab/BottomTabBar.tsx
@@ -3,14 +3,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeStackScreen from '../stack/HomeStack/HomeStackScreen';
 import ContactScreen from '../../screens/contact/ContactScreen';
 import { BottomTabParamList } from '../types';
+import { DrawerScreenWrapper } from '../../components';
 
 const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabBar() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={HomeStackScreen} />
-      <Tab.Screen name="Contact" component={ContactScreen} />
-    </Tab.Navigator>
+    <DrawerScreenWrapper>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Contact" component={ContactScreen} />
+      </Tab.Navigator>
+    </DrawerScreenWrapper>
   );
 }
--- src/navigation/drawer/SideDrawer.tsx
+++ src/navigation/drawer/SideDrawer.tsx
@@ -1,15 +1,50 @@
 import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  DrawerContentComponentProps,
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTabBar from '../bottomTab/BottomTabBar';
 import { DrawerParamList } from '../types';
+import { colors } from '../../constant/colors';
+import { DrawerContent } from '../../components';
 
 const Drawer = createDrawerNavigator<DrawerParamList>();
+
+const CustomDrawer = (props: DrawerContentComponentProps) => (
+  <DrawerContent {...props} />
+);
 
 export default function SideDrawer() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Start">
+      <Drawer.Navigator
+        drawerContent={CustomDrawer}
+        initialRouteName="Start"
+        screenOptions={{
+          drawerType: 'back',
+          headerShown: false,
+          overlayColor: 'transparent',
+          drawerActiveBackgroundColor: `${colors.secondary}50`,
+          drawerActiveTintColor: colors.secondary,
+          drawerLabelStyle: {
+            fontSize: 16,
+          },
+          drawerItemStyle: {
+            borderRadius: 8,
+            paddingHorizontal: 8,
+          },
+          drawerStyle: {
+            backgroundColor: colors.white,
+            width: '50%',
+          },
+          drawerContentContainerStyle: {
+            backgroundColor: colors.primary,
+          },
+          sceneContainerStyle: {
+            backgroundColor: colors.primary,
+          },
+        }}>
         <Drawer.Screen name="Start" component={BottomTabBar} />
       </Drawer.Navigator>
     </NavigationContainer>
